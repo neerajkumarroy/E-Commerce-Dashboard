757b87dcc57e054060b6b517cfb35699f579ac4a
neerajkumarroy <neerajkumarroyss@gmail.com>
2024-03-02 11:18:06 +0530

    add file

--- backend/DB/config.js
+++ backend/DB/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_URL)
 .then(()=>{
-    console.log("DataBase connect Succsfully..........!");
+    console.log("DataBase connect Successfully..........!");
 }).catch((err)=>{
     console.log(err);
 });
